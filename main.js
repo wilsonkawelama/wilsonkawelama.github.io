d2f42de23a465b2d071d0549d43ebd485935b3d8
WILSON <145309233+wilsonkawelama@users.noreply.github.com>
2025-03-31 07:44:19 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = () =>{
+
+const successCallback = (position) =>{
+    console.log(position);
+}
+const errorCallback = (error) =>{
+    console.log(error);
+}
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+}
